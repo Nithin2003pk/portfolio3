acf70f45d3f3225f8ca6cb3e7802ae14130335b4
Nithin P K <nithinpk136@gmail.com>
2026-03-09 22:01:12 +0530

    email setup

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -132,7 +132,7 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
 EMAIL_HOST_USER = 'nithinpk136@gmail.com'
-EMAIL_HOST_PASSWORD = 'your_app_password'
+EMAIL_HOST_PASSWORD = 'odex cfpj czek szdr'
 
 import os
 
